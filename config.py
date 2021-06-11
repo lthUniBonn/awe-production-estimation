012a1bf4880cdcad32f2b22587a3630cda3aeefa
lthUniBonn <lthimm@uni-bonn.de>
2021-06-11 13:31:48 +0200

    add config options for aep/power curves - output file names

--- config.py
+++ config.py
@@ -2,22 +2,21 @@
 #TODO remove old
 
 # Clustering chain
+
 # File name definitions
-from config_clustering import file_name_profiles, file_name_freq_distr, cut_wind_speeds_file, cluster_config, data_info
-# old frequency file: wind_speed_probability_file = "wind_resource/freq_distribution_v3{}.pickle"
+from config_clustering import file_name_profiles, file_name_freq_distr, cut_wind_speeds_file, data_info, result_dir, plots_interactive, \
+    n_clusters
+
+#optimizer:
+optimizer_history_file_name = result_dir + 'optimizer_history_{data_info}.hist'.format(data_info=data_info)
 
 # power curves:
-#power_curve_output_file_name = 'output/power_curve_{{i_profile}}_{cluster_config}{data_info}.{{suffix}}'.format(
-#    cluster_config=cluster_config, data_info=data_info) # suffix= csv / pickle
-power_curve_output_file_name = 'output/power_curve_{{i_profile}}_{cluster_config}{data_info}_successful.{{suffix}}'.format(
-    cluster_config=cluster_config, data_info=data_info) # suffix= csv / pickle
-#old: power_curve_file = 'output/power_curve{}{}.csv'.format(suffix,i_profile)
-
-# old (for each cluster id i_profile): 'wind_resource/'+'profile{}{}.csv'.format(suffix, i_profile), sep=";"
+run_marker = '_opt-successful-test' # default = ''
+power_curve_output_file_name = result_dir + 'power_curve_{{i_profile}}_{data_info}{run_info}_filtered.{{suffix}}'.format(
+    data_info=data_info, run_info=run_marker) # suffix= csv / pickle
 
 refined_cut_wind_speeds_file = cut_wind_speeds_file.replace('estimate', 'refined')
-#  cut-in/out wind speeds file:
-#old: 'output/wind_limits_estimate{}.csv'.format(suffix)
-#old: 'output/wind_limits_refined{}.csv'.format(suffix)
+
+plot_output_file = result_dir + '{title}' + data_info + '.pdf'
 
 
